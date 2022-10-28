7c09959a414bce21c7336cb6a2518c9f76926210
margauxargentin <margauxargentin@gmail.com>
2022-10-28 09:08:23 +0200

    joueur

---
+++ src/main/java/Personne.java
@@ -0,0 +1,19 @@
+
+public class Personne {
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	
+	public Personne(int id, String nom, String prenom) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+
+}
