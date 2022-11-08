9dc76b11aa9e28d951e9a9e3ffc6f27f2c6f9ceb
Sunlestial <katell.quemeneur@gmail.com>
2022-11-08 14:47:36 +0100

    Ajout interface et modèle joueur

---
+++ src/main/java/ListeJoueurServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet ("/joueurs")
+public class ListeJoueurServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+
+        out.println("Liste des joueurs!");
+
+  }    
+
+}
