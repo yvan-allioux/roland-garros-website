92118011ba514b2eeb4ff6ea732e138be2e8bb15
margauxargentin <margauxargentin@gmail.com>
2022-11-08 14:51:06 +0100

    Création du la classe Equipe

---
+++ src/main/java/Equipe.java
@@ -0,0 +1,15 @@
+public class Equipe{
+	
+	private int id;
+	private Joueur joueur1, joueur2;
+	
+	public Equipe(int id) {
+		this.id = id;
+	}
+	
+	public void setJoueurs(Joueur joueur1, Joueur joueur2) {
+		joueur1 = joueur1;
+		joueur2 = joueur2;
+	}
+	
+}
